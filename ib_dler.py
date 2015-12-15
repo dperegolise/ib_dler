cdb56a98ef0360e41c601b8ac9889d6821a9f0dc
daniel peregolise <danielperegolise@gmail.com>
2015-12-15 01:13:16 -0500

    move stdout messages to container class

--- ib_dler.py
+++ ib_dler.py
@@ -2,27 +2,52 @@ import re
 import sys
 import urllib.request
 
+# Custom Exception classes
+
+class ArgumentException(Exception):
+    pass
+
+
+# Resource container classes
+
+class Messages:
+    # Help Prompt
+    def HELP():
+        return """Image Board Downloader (Fri Dec 11 21:52:52 EST 2015)
+ibdl [OPTION] URL OUTPUTDIR
+    The default action is to identify every anchor found at URL whose reference contains an
+    image extension type, and download it to OUTPUTDIR.
+    -e      [TODO] Extension list in the form of ext1,ext2...
+    -h      [TODO] Show response headers
+    -b      [TODO] Show response body"""
+
+    # Argument info
+    def ECHOENV(url, od) :
+        return 'Using URL: ' + url + '\nUsing Output Dir: ' + od + '\n'
+
+    # HTTP headers
+    def HEADERS(r, url):
+        return str(r.info()) + '\n' + 'URL Fetched: ' + url
+
+
 def main():
     ''' Main function, where all the fun happens'''
 
     # Check for human error
     if len(sys.argv) < 3:
-        raise Exception('URL and output dir arguments are required.')
+        raise ArgumentException('URL and output dir arguments are required.')
 
     # Grab arguments from sysv
     url, od = sys.argv[1], sys.argv[2]
     if not od.endswith('/'):
         od += '/'
-    print('Using URL: ' + url)
-    print('Using Output Dir: ' + od)
-    print()
+    print(Messages.ECHOENV(url, od))
 
     # send GET to URL
     try:
         r = urllib.request.urlopen(url)
         response = r.read()
-        print(r.info())
-        print("URL Fetched: " + url)
+        print(Messages.HEADERS(r, url))
     except Exception as e:
         raise Exception('Error occured during initial request: ' \
             + type(e).__name__ + ': ' + str(e))
@@ -59,18 +84,13 @@ def main():
             n += 1
 
 
-
-
 if __name__ == '__main__':
     try:
-        if sys.argv[1] == '--help':
-            print("""Image Board Downloader (Fri Dec 11 21:52:52 EST 2015)
-ibdl [OPTION] URL OUTPUTDIR
-    The default action is to identify every anchor found at URL whose reference contains an
-    image extension type, and download it to OUTPUTDIR.
-    -e      [TODO] Extension list in the form of ext1,ext2...
-    -h      [TODO] Show request headers""")
+        if len(sys.argv) == 1 or sys.argv[1] == '--help':
+            print(Messages.HELP())
         else:
             main()
+    except ArgumentException as ae:
+        print(str(ae) + '\n\n' + Messages.HELP())
     except Exception as e:
-        print(type(e).__name__ + ': ' + str(e))
+        print(str(e))
